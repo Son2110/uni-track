f9a645b9f7fb4cbed64c491363d1e8c51ed0e39d
haibeu2901 <beu2901@gmail.com>
2026-01-31 00:36:23 +0700

    refactor: re-design graphql implementation to ensure clean architech and solid design

--- backend/PMSS.Application/Interfaces/Repositories/IGenericRepository.cs
+++ backend/PMSS.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -6,6 +6,7 @@ public interface IGenericRepository<T> where T : class
 {
     Task<T?> GetByIdAsync(Guid id);
     Task<IEnumerable<T>> GetAllAsync();
+    IQueryable<T> GetAllQueryable();
     Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
     Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
     Task AddAsync(T entity);
